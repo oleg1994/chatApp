f28cdf81ebdca3d4854bbe19dd0c5fd51be46daf
Oleg M <emerlenderz@gmail.com>
2020-05-14 17:36:40 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Button, DatePicker } from "antd";
+import "antd/dist/antd.css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <Button type="primary" style={{ marginLeft: 8 }}>hello</Button>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
